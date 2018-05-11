765b364ecec595e9dd52c7fa63f02563430c0ce4
Jes Ababon <jesababon@gmail.com>
2018-05-10 21:04:08 -0400

    added countdown

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ $(function () {
     //add objects
     const get = ["🎒", "📱", "🔑", "☕️", "💻", "👓", "🎧", "🍩", "📓", "💊"];
     const misc = ['🏈', '📚', '🍕', '🎨', '⌚️', '⌛️', '💡', '💰', '🔧 ', '📼', '🔬', '✂️ ', '🖍', '🔮', '📿'];
+    const clock = $('.clock');
+    let countdown = 10;
 
     //add playField const
     const $playField = $('.play-field');
@@ -19,8 +21,8 @@ $(function () {
         let $addGet = $playField.append($gottaGet);
 
         //help with randomization src: http://jsfiddle.net/hspvadfv/
-        var x = Math.max(0, Math.min(60, Math.ceil(Math.random() * 100)));
-        var y = Math.max(0, Math.min(60, Math.ceil(Math.random() * 100)));
+        var x = Math.max(0, Math.min(100, Math.ceil(Math.random() * 100)));
+        var y = Math.max(0, Math.min(100, Math.ceil(Math.random() * 100)));
 
 
         $gottaGet.css({
@@ -28,13 +30,13 @@ $(function () {
             top: y + '%',
             left: x + '%',
         });
-        return [];
 
 
     }
 
     function makeMisc() {
-        const $misc = $('<div class="item"></div>').append(misc[Math.floor(Math.random() * misc.length)]);
+        const $misc = $('<div class="noclick"></div>').append(misc[Math.floor(Math.random() * misc.length)]);
+
 
 
         let $notNeeded = $playField.append($misc);
@@ -48,16 +50,19 @@ $(function () {
         $misc.css({
             position: 'absolute',
             top: y + '%',
-            left: x + '%',
+            left: x + '%', 
+            
         });
     }
     //add multiple objects to playfield and randomize their starting place.
 
     function manyMisc() {
-        let left = Math.random() * window.innerWidth;
-        let top = Math.random() * window.innerHeight;
+        let left = Math.random() * $playField.innerWidth;
+        let top = Math.random() * $playField.innerWidt;
 
-        for (let i = 0; i < 30; i++) {
+
+
+        for (let i = 0; i < 10; i++) {
 
             makeMisc();
 
@@ -65,71 +70,67 @@ $(function () {
     }
 
      function manyEssentials() {
-       let left = Math.random() * window.innerWidth;
-       let top = Math.random() * window.innerHeight;
+       let left = Math.random() * $playField.innerWidth;
+       let top = Math.random() * $playField.innerHeight;
 
 
        for (let i = 0; i < 5; i++) {
 
          createEssentials();
 
-
-
-
        }
      }
 
+//add function removing objects from the field
+function pickThingsUp() {
+  $('.item').on('click', function () {
+    $(this).remove();
+
+  });
+}
+
+//add countdown clock
+function startClock() {
+  let $interval = setInterval(function time() {
+    console.log(countdown);
+    if (countdown !== 0) {
+      countdown--;
+      $('.clock').html(`${countdown} seconds <p>remaining</p>`);
+    } else {
+      clearInterval($interval);
+      //end game function
+    }
+  }, 1000);
+}
+
+//add game initialization function
 function startGame (){
 manyEssentials();
-manyMisc();}
+manyMisc();
+startClock();
+pickThingsUp();
+}
+
+
 
 startGame();
 
 
-function pickThingsUp (){
-$('.item').on('click', function () {
-  $(this).remove();
-  
-});}
+//BONUS add <div>Collected</div> and put selected objects in this div 
 
 
+//add function of winnerWinner if all objects are removed
 
 
-//add function removing objects from the field
-// function pickUp(){
-//     div.forEach(element =>
-//        {element.addEventListener('click', function(event){
-//            event.target.classList('item');
-//            setInterval(function remove() {
-//              event.target.remove();
-//            }, 250);
-//         //    score += 1;
-//         //    setTimeout(checkForWinner, 500);
-
-//        });
-//     });
-// }
+//add else function of youAreLate if all objects are not removed
 
 
+//add exitDoor function to initiate winnerWinner function
 
 
+//add reset game button (or set a object reset button inside the collected div)
 
-    //BONUS add <div>Collected</div> and put selected objects in this div 
-
-
-    //add function of winnerWinner if all objects are removed
-
-    //add Timer
-
-    //add else function of youAreLate if all objects are not removed
-
-
-    //add exitDoor function to initiate winnerWinner function
-
-
-    //add reset game button (or set a object reset button inside the collected div)
-
-    //build next level and link the two with localStorage
+//build next level and link the two with localStorage
 
 
 });
