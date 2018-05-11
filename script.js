73d66bdd3549177f945aa34a25573abec7279a01
Jes Ababon <jesababon@gmail.com>
2018-05-10 23:19:37 -0400

    played with x y to better fit divs to window

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-
-
 $(function () {
 
     //add objects
@@ -7,6 +5,7 @@ $(function () {
     const misc = ['🏈', '📚', '🍕', '🎨', '⌚️', '⌛️', '💡', '💰', '🔧 ', '📼', '🔬', '✂️ ', '🖍', '🔮', '📿'];
     const clock = $('.clock');
     let countdown = 10;
+    
 
     //add playField const
     const $playField = $('.play-field');
@@ -21,8 +20,9 @@ $(function () {
         let $addGet = $playField.append($gottaGet);
 
         //help with randomization src: http://jsfiddle.net/hspvadfv/
-        var x = Math.max(0, Math.min(100, Math.ceil(Math.random() * 100)));
-        var y = Math.max(0, Math.min(100, Math.ceil(Math.random() * 100)));
+        
+        let x = Math.max(0, Math.min(80, Math.ceil(Math.random() * 90)));
+        let y = Math.max(0, Math.min(80, Math.ceil(Math.random() * 90)));
 
 
         $gottaGet.css({
@@ -36,47 +36,109 @@ $(function () {
 
     function makeMisc() {
         const $misc = $('<div class="noclick"></div>').append(misc[Math.floor(Math.random() * misc.length)]);
-
-
-
         let $notNeeded = $playField.append($misc);
 
-        //Added x and y randoms to the create function. then force the position to css.
 
-        var x = Math.max(0, Math.min(60, Math.ceil(Math.random() * 100)));
-        var y = Math.max(0, Math.min(60, Math.ceil(Math.random() * 100)));
+        //Added x and y randoms to the create function. then force the position to css.
+        let x = Math.max(0, Math.min(60, Math.ceil(Math.random() * 100)));
+        let y = Math.max(0, Math.min(60, Math.ceil(Math.random() * 100)));
+
+
 
 
         $misc.css({
             position: 'absolute',
             top: y + '%',
             left: x + '%', 
-            
+
+            //looking for overlapping solutions. found http://jsfiddle.net/AHReF/1/
+
+        // let coords = {
+        //   w: $(this).outerWidth(true),
+        //   h: $(this).outerHeight(true)
+        // };
+        // let success = false;
+        // while (!success) {
+        //   coords.x = parseInt(Math.random() * ($playField - coords.w));
+        //   coords.y = parseInt(Math.random() * ($playField - coords.h));
+        //   let success = true;
+        //   $.each(positions, function () {
+        //     if (
+        //       coords.x <= (this.x + this.w) &&
+        //       (coords.x + coords.w) >= this.x &&
+        //       coords.y <= (this.y + this.h) &&
+        //       (coords.y + coords.h) >= this.y
+        //     ) {
+        //       success = false;
+        //     }
+        //   });
+        // }
+        // positions.push(coords);
+        // $(this).css({
+        //       top: coords.y + 'px',
+        //       left: coords.x + 'px'
+        //above logic would not process. kept looping.
+        //overlap tutorial in p5.js https://www.youtube.com/watch?v=XATr_jdh-44
+        //distance is greater than radius 1 + radius of 2. distance function.
+        // let protection = 0;
+        // const spaceBetween = [];
+
+        // while (spaceBetween.length < 20) {
+        //   let manyPos = {
+        //     x: Math.random() * $playField,
+        //     y: Math.random() * $playField,
+
+
+        //   };
+
+        //   let overlapping = false;
+        //   for (let j = 0; j < spaceBetween.length; j++) {
+        //     let other = spaceBetween[j];
+        //     let a = manyPos.x - other.x;
+        //     let b = manyPos.y - other.y;
+        //     let d = Math.sqrt(((a * a) + (b * b)));
+        //     if (d < 30) {
+        //       overlapping = true;
+        //     }
+        //   }
+
+
+        //   if (!overlapping) {
+        //     spaceBetween.push(manyPos);
+        //   }
+        //   protection++;
+        //   if (protection > 10) {
+        //     break;
+        //   }
+        //   for (let i = 0; i < spaceBetween.length; i++) {
+        //     makeMisc(spaceBetween[i].x, spaceBetween[i].y);           
         });
     }
     //add multiple objects to playfield and randomize their starting place.
 
+
+    
     function manyMisc() {
-        let left = Math.random() * $playField.innerWidth;
-        let top = Math.random() * $playField.innerWidt;
-
-
-
         for (let i = 0; i < 10; i++) {
+          let manyPos = {
+            x: Math.random() * window.innerWidth,
+            y: Math.random() * window.innerHeight,
+          };
 
-            makeMisc();
+          makeMisc();        
 
         }
     }
 
      function manyEssentials() {
-       let left = Math.random() * $playField.innerWidth;
-       let top = Math.random() * $playField.innerHeight;
-
-
-       for (let i = 0; i < 5; i++) {
+       for (let i = 0; i < 20; i++) {
+         let manyPos = {
+           x: Math.random() * window.innerWidth,
+           y: Math.random() * window.innerHeight,
+         };
 
          createEssentials();
+
 
        }
      }
@@ -116,13 +178,15 @@ pickThingsUp();
 startGame();
 
 
-//BONUS add <div>Collected</div> and put selected objects in this div 
-
-
 //add function of winnerWinner if all objects are removed
 
 
-//add else function of youAreLate if all objects are not removed
+//add <div>Collected</div> and put selected objects in this div 
+
+//get divs to remain in $playfield. Mike suggested creating a grid and divs populate in ranodm cells.
+
+
+//add else function of youAreLate if all objects are not removed on time
 
 
 //add exitDoor function to initiate winnerWinner function
@@ -132,5 +196,8 @@ startGame();
 
 //build next level and link the two with localStorage
 
+//build landing page
+
 
 });
+
